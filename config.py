5db615fc3909184aef342e1503ff3c378ee63949
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2022-07-11 22:40:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,21 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+que = {}
+admins = {}
+
+API_ID = int(getenv("API_ID", "9222289"))
+API_HASH = getenv("API_HASH", "2777b770a525c2072cf270bac1e99aca)
+BOT_TOKEN = getenv("BOT_TOKEN", None)
+BOT_NAME = getenv("BOT_NAME","Ɗᴇᴇᴘᴀɴꜱʜᴜ ᴍᴜsɪᴄ ʙᴏᴛ")
+BOT_USERNAME = getenv("BOT_USERNAME", "Deepanshu_Music_Robot")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "DEEPANSHU_KI_DUNIYA")
+DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
+START_IMG = getenv("START_IMG", "https://telegra.ph/file/89cbc8b8760b6abff430f.jpg")
+PING_IMG = getenv("PING_IMG", "https://telegra.ph/file/89cbc8b8760b6abff430f.jpg")
+SESSION_NAME = getenv("SESSION_NAME", None)
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "? ~ + • / ! ^ .").split())
+PMPERMIT = getenv("PMPERMIT", "ENABLE")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1572358619").split()))
